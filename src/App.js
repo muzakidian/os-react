2716cd361e71e4cc2ffdbcd326d19ff3afd3fa9f
muzakidian <muzakidian1@gmail.com>
2021-11-24 00:59:01 +0700

    third

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Halo, Selamat siang saat ini kita sedang belajar CI/CD TI-3H</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
